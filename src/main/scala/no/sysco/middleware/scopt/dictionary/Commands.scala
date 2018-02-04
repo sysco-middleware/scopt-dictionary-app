9b8110b068cdef31e44bd82ebac063bfbdea6562
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-02-04 15:05:14 +0100

    add CRUD strategy view (done with READ)

--- src/main/scala/no/sysco/middleware/scopt/dictionary/Commands.scala
+++ src/main/scala/no/sysco/middleware/scopt/dictionary/Commands.scala
@@ -3,30 +3,62 @@ package no.sysco.middleware.scopt.dictionary
 import java.util.Optional
 
 
-
+/**
+  * Commands: CRUD
+  * C - add
+  * R - show
+  * U - update
+  * D - delete
+  */
 object Commands {
-  final val STORAGE = s"STORAGE"
-  final val SHOW = s"SHOW"
-  final val ADD = s"ADD"
+  final val STORAGE : String = s"storage"
+
+  final val ADD : String = s"add"
+  final val SHOW : String= s"show"
+  final val UPDATE : String= s"update"
+  final val DELETE : String= s"delete"
+
+  final val ORDER_ASC : String = s"asc"
+  final val ORDER_DESC : String= s"desc"
+
 
   val parser = new scopt.OptionParser[Command]("dictionary") {
     head("dictionary", "1.0")
-
-    cmd("storage")
-      .action((_,c) => c.copy(action = Optional.of(STORAGE)))
+    cmd(STORAGE)
+      .action((_,c) => c.copy(action = Option.apply(STORAGE)))
       .text(s"print current dictionary file")
 
-    cmd("show")
-        .action((_,c) => c.copy(action = Optional.of(SHOW)))
-        .text(s"show all words in dictionary")
+    cmd(ADD)
+      .action((_,c)=> c.copy(action = Option.apply(ADD)))
+      .text(s"add key:value pair")
+      .children(
+//        opt[Map[String,String]]("kwargs").valueName("k1=v1,k2=v2...").action( (x, c) =>
+//          c.copy(kwargs = x) ).text("other arguments")
+      )
 
-//    cmd("add")
-//      .action((_,c) => c.copy(action = Optional.of(ADD)))
-//      .text(s"add new word:translation to dictionary")
-//      .children(
-//        opt[String]("key")
-//
-//      )
+
+    cmd(SHOW)
+      .action((_,c) => c.copy(action = Option.apply(SHOW)))
+      .text(s"show all words")
+        .children(
+          // flags
+          opt[Unit]("ascending")
+            .abbr("asc")
+            .action((_,c)=> c.copy(action = Option.apply(SHOW), options = Option.apply(ORDER_ASC)))
+            .text(s"ASC order"),
+          opt[Unit]("descending")
+            .abbr("desc")
+            .action((_,c)=> c.copy(action = Option.apply(SHOW), options = Option.apply(ORDER_DESC)))
+            .text(s"DESC order")
+        )
+
+
+    cmd(UPDATE)
+      .action((_,c) => c.copy(action = Option.apply(UPDATE)))
+      .text(s"replace key:value entry")
+    cmd(DELETE)
+      .action((_,c) => c.copy(action = Option.apply(DELETE)))
+      .text(s"delete key:value entry")
 
     help("help").abbr("h").text("Show usage")
     // TODO: only for test
@@ -35,4 +67,4 @@ object Commands {
 
 }
 
-case class Command(action: Optional[String] = Optional.empty(), options: Optional[Seq[String]]= Optional.empty())
+case class Command(action: Option[String] = Option.empty, options: Option[String]= Option.empty)
--- src/main/scala/no/sysco/middleware/scopt/dictionary/DictionaryService.scala
+++ src/main/scala/no/sysco/middleware/scopt/dictionary/DictionaryService.scala
@@ -6,17 +6,19 @@ class DictionaryService(config: Config) {
   import no.sysco.middleware.scopt.dictionary.Commands._
 
   def build(command: Command) : Unit = {
-
-    command.action.get() match {
-      case STORAGE => println(config.dictionary.storage)
-      case SHOW => show()
-    }
-
-  }
-
-  private def show() : Unit = {
-    for (line <- Source.fromFile(config.dictionary.storage).getLines) {
-      println(line)
+    command.action match {
+      case Some(STORAGE) => println(config.dictionary.storage)
+      case Some(SHOW) => show(command)
     }
   }
+
+  private def show(command: Command) : Unit = {
+    val source = Source.fromFile(config.dictionary.storage)
+    command.options match {
+      case None => for (line <- source.getLines) println(line)
+      case Some(ORDER_ASC) => source.getLines.toList.sortWith(_ < _).foreach(el => println(el))
+      case Some(ORDER_DESC) => source.getLines.toList.sortWith(_ > _).foreach(el => println(el))
+    }
+  }
+
 }
